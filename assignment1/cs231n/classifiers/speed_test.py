171e337a1160f9ee4ab5039c8d250c04917f5483
ds18b20 <858362490@qq.com>
2018-05-25 21:54:34 +0900

    20180525 knn

--- assignment1/cs231n/classifiers/speed_test.py
+++ assignment1/cs231n/classifiers/speed_test.py
@@ -6,24 +6,27 @@ def timeit(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.time()
-        func(*args, **kwargs)
+        ret = func(*args, **kwargs)
         elapsed_time = time.time() - start_time
         print('function [{}] finished in {} ms'.format(
             func.__name__, int(elapsed_time * 1000)))
+        return ret
     return wrapper
 
 @timeit
 def mul(a, b):
     row = a.shape[0]
-    return np.sum(a.reshape(row, 1, -1) * b, axis=-1)
+    ret = np.sum(a.reshape(row, 1, -1) * b, axis=-1)
+    return ret
     
 @timeit
 def mul_dot(a, b):
-    return np.dot(a, b.T)
+    ret = np.dot(a, b.T)
+    return ret
     
 if __name__ == "__main__":
     a = np.random.randn(50, 32*32*3)
     b = np.random.randn(500, 32*32*3)
     
-    mul(a, b)
-    mul_dot(a, b)
+    print('Return Array[0][0]', mul(a, b)[0][0])
+    print('Return Array[0][0]', mul_dot(a, b)[0][0])
